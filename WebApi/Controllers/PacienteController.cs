fd3df4e0a62e19d39965ce1abc9d18af24edb16f
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-01-06 23:17:59 -0300

    #20 - Criado todos os links para a controller paciente

--- WebApi/Controllers/PacienteController.cs
+++ WebApi/Controllers/PacienteController.cs
@@ -21,6 +21,7 @@ public class PacienteController : ControllerBase
         _service = service;
     }
 
+    #region Paciente
     [HttpGet("ListaPacientesClinica/{idClinica:int}")]
     [Produces("application/json")]
     public async Task<object> ListaPacientesClinica(int idClinica) => await _repository.ListaPacienteClinica(idClinica);
@@ -41,7 +42,7 @@ public class PacienteController : ControllerBase
             RG = pacienteDTO.RG,
             EstadoCivil = pacienteDTO.EstadoCivil,
             Profissao = pacienteDTO.Profissao,
-            IdClinica = pacienteDTO.IdClinica   
+            IdClinica = pacienteDTO.IdClinica
         };
 
         ContatoPaciente contato = new ContatoPaciente
@@ -101,4 +102,149 @@ public class PacienteController : ControllerBase
         await _service.DeletarPaciente(idPaciente);
         return Ok();
     }
+    #endregion
+
+    #region Endereco
+    [HttpGet("ListaEnderecoPaciente/{idPaciente:int}")]
+    [Produces("application/json")]
+    public async Task<object> ListaEnderecoPaciente(int idPaciente) => 
+        await _service.ListaEnderecosPaciente(idPaciente);
+
+    [HttpGet("ObterEnderecoPaciente/{idEndereco:int}")]
+    [Produces("application/json")]
+    public async Task<ActionResult<EnderecoPaciente>> ObterEnderecoPaciente(int idEndereco) => 
+        await _service.ObterEnderecoPaciente(idEndereco);
+
+    [HttpPost("AdicionarEnderecoPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AdicionarEnderecoPaciente(EnderecoPaciente endereco)
+    {
+        await _service.AdicionarEnderecoPaciente(endereco);
+        return Ok();
+    }
+
+    [HttpPut("AtualizarEnderecoPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AtualizarEnderecoPaciente(EnderecoPaciente endereco)
+    {
+        await _service.AtualizarEnderecoPaciente(endereco);
+        return Ok();
+    }
+
+    [HttpDelete("DeletarEnderecoPaciente/{idEndereco:int}")]
+    [Produces("application/json")]
+    public async Task<IActionResult> DeletarEnderecoPaciente(int idEndereco)
+    {
+        await _service.DeletarEnderecoPaciente(idEndereco);
+        return Ok();
+    }
+    #endregion
+
+    #region Contato
+    [HttpGet("ListaContatoPaciente/{idPaciente:int}")]
+    [Produces("application/json")]
+    public async Task<object> ListaContatoPaciente(int idPaciente) =>
+        await _service.ListaContatoPaciente(idPaciente);
+
+    [HttpGet("ObterContatoPaciente/{idContato:int}")]
+    [Produces("application/json")]
+    public async Task<ActionResult<ContatoPaciente>> ObterContatoPaciente(int idContato) =>
+        await _service.ObterContatoPaciente(idContato);
+
+    [HttpPost("AdicionarContatoPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AdicionarContatoPaciente(ContatoPaciente contato)
+    {
+        await _service.AdicionarContatoPaciente(contato);
+        return Ok();
+    }
+
+    [HttpPut("AtualizarContatoPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AtualizarContatoPaciente(ContatoPaciente contato)
+    {
+        await _service.AtualizarContatoPaciente(contato);
+        return Ok();
+    }
+
+    [HttpDelete("DeletarContatoPaciente/{idContato:int}")]
+    [Produces("application/json")]
+    public async Task<IActionResult> DeletarContatoPaciente(int idContato)
+    {
+        await _service.DeletarContatoPaciente(idContato);
+        return Ok();
+    }
+    #endregion
+
+    #region Convenio
+    [HttpGet("ListaConvenioPaciente/{idPaciente:int}")]
+    [Produces("application/json")]
+    public async Task<object> ListaConvenioPaciente(int idPaciente) =>
+        await _service.ListaConveniosPaciente(idPaciente);
+
+    [HttpGet("ObterConvenioPaciente/{idConvenio:int}")]
+    [Produces("application/json")]
+    public async Task<ActionResult<PacienteConvenio>> ObterConvenioPaciente(int idConvenio) =>
+        await _service.ObterPacienteConvenio(idConvenio);
+
+    [HttpPost("AdicionarConvenioPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AdicionarConvenioPaciente(PacienteConvenio Convenio)
+    {
+        await _service.AdicionarPacienteConvenio(Convenio);
+        return Ok();
+    }
+
+    [HttpPut("AtualizarConvenioPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AtualizarConvenioPaciente(PacienteConvenio Convenio)
+    {
+        await _service.AtualizarPacienteConvenio(Convenio);
+        return Ok();
+    }
+
+    [HttpDelete("DeletarConvenioPaciente/{idConvenio:int}")]
+    [Produces("application/json")]
+    public async Task<IActionResult> DeletarConvenioPaciente(int idConvenio)
+    {
+        await _service.DeletarPacienteConvenio(idConvenio);
+        return Ok();
+    }
+    #endregion
+
+    #region Familiar
+    [HttpGet("ListaFamiliarPaciente/{idPaciente:int}")]
+    [Produces("application/json")]
+    public async Task<object> ListaFamiliarPaciente(int idPaciente) =>
+        await _service.ListaPacienteFamiliar(idPaciente);
+
+    [HttpGet("ObterFamiliarPaciente/{idFamiliar:int}")]
+    [Produces("application/json")]
+    public async Task<ActionResult<PacienteFamiliar>> ObterFamiliarPaciente(int idFamiliar) =>
+        await _service.ObterPacienteFamiliar(idFamiliar);
+
+    [HttpPost("AdicionarFamiliarPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AdicionarFamiliarPaciente(PacienteFamiliar Familiar)
+    {
+        await _service.AdicionarPacienteFamiliar(Familiar);
+        return Ok();
+    }
+
+    [HttpPut("AtualizarFamiliarPaciente")]
+    [Produces("application/json")]
+    public async Task<IActionResult> AtualizarFamiliarPaciente(PacienteFamiliar Familiar)
+    {
+        await _service.AtualizarPacienteFamiliar(Familiar);
+        return Ok();
+    }
+
+    [HttpDelete("DeletarFamiliarPaciente/{idFamiliar:int}")]
+    [Produces("application/json")]
+    public async Task<IActionResult> DeletarFamiliarPaciente(int idFamiliar)
+    {
+        await _service.DeletarPacienteFamiliar(idFamiliar);
+        return Ok();
+    }
+    #endregion
 }
