f7eed6db6c1fe79484b638696ce87dd980029336
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-01-24 21:39:31 -0300

    NMB-18 | Criação do model PlanosConvenio

---
+++ Enities/Models/PlanosConvenio.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Entities.Models;
+
+public class PlanosConvenio : Base
+{
+
+    [ForeignKey("Convenio")]
+    [Column(Order = 1)]
+    public int IdConvenio { get; set; }
+    [JsonIgnore]
+    public virtual Convenio? Convenio { get; set; }
+}
