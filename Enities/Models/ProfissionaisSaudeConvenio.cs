184064dd0af8a4f90470cab523a0bed54f586ed6
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-01-24 21:42:51 -0300

    NMB-20 | Criação do Model Profissionais Saude Convenio

---
+++ Enities/Models/ProfissionaisSaudeConvenio.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Entities.Models;
+
+public class ProfissionaisSaudeConvenio
+{
+
+    [ForeignKey("Convenio")]
+    [Column(Order = 1)]
+    public int IdConvenio { get; set; }
+    [JsonIgnore]
+    public virtual Convenio? Convenio { get; set; }
+
+    [ForeignKey("Funcionario")]
+    [Column(Order = 1)]
+    public int IdFuncionario { get; set; }
+    [JsonIgnore]
+    public virtual Funcionario? Funcionario { get; set; }
+}
