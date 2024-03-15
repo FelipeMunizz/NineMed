c86de7481cf9b55f3990d72c8ecc16942e6ce728
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-03-14 21:51:01 -0300

    Tornar possivel nulos na configuracao financeira

--- Enities/Models/ConfiguracaoFinanceira.cs
+++ Enities/Models/ConfiguracaoFinanceira.cs
@@ -8,27 +8,27 @@ public class ConfiguracaoFinanceira
     public int Id { get; set; }
 
     [ForeignKey("Banco")]
-    public int IdBanco { get; set; }
+    public int? IdBanco { get; set; }
     [JsonIgnore]
     public virtual Banco? Banco { get; set; }
 
     [ForeignKey("ContaBancaria")]
-    public int IdContaBancaria { get; set; }
+    public int? IdContaBancaria { get; set; }
     [JsonIgnore]
     public virtual ContaBancaria? ContaBancaria { get; set; }
 
     [ForeignKey("CentroCusto")]
-    public int IdCentroCusto { get; set; }
+    public int? IdCentroCusto { get; set; }
     [JsonIgnore]
     public virtual CentroCusto? CentroCusto { get; set; }
 
     [ForeignKey("FormaPagamento")]
-    public int IdFormaPagamento { get; set; }
+    public int? IdFormaPagamento { get; set; }
     [JsonIgnore]
     public virtual FormaPagamento? FormaPagamento { get; set; }
 
     [ForeignKey("Clinica")]
-    public int IdClinica { get; set; }
+    public int? IdClinica { get; set; }
     [JsonIgnore]
     public virtual Clinica? Clinica { get; set; }
 }
